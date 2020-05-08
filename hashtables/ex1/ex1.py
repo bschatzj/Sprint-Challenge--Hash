af8e857aa6bea2f92ec3ebcb604c241aeee3ba8d
Brendan <brendanschatz97@gmail.com>
2020-05-08 12:06:53 -0400

    q1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,19 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
 
+    weights_dict = dict()
+
+    for index in range(length):
+        # find the index that gets us to the limit
+        index_two = weights_dict.get(limit - weights[index])
+        
+        if index_two is not None:
+
+            print(limit)
+            print(index)
+            print(index_two)
+            return (index, index_two)
+        else:
+            weights_dict[weights[index]] = index
+
     return None
